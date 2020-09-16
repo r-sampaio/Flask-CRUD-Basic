f4d428d4346bd3854ae2bde22b28cc2b3595a001
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-15 21:56:01 -0300

    Create cliente_models.py

---
+++ one/app/models/cliente_models.py
@@ -0,0 +1,18 @@
+from app import db
+from sqlalchemy_utils import ChoiceType
+
+class Cliente(db.Model):
+    __tablename__ = "clientes"
+    
+    SEXO_CHOICES = [
+        (u'M', u'Masculino'),
+        (u'F', u'Feminino'),
+        (u'N', u'Nenhuma das opções')
+    ]
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(200), unique=True)
+    data_nascimento = db.Column(db.DateTime)
+    profissao = db.Column(db.String(30))
+    sexo = db.Column(ChoiceType(SEXO_CHOICES))
