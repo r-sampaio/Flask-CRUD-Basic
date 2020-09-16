c283ac372e155f7ec72ca0fe53239c632701a4f2
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-15 21:27:26 -0300

    Create config-example.py

---
+++ one/config-example.py
@@ -0,0 +1,9 @@
+DEBUG = True or False
+
+USERNAME = 'user'
+PASSWORD = 'pass'
+SERVER = 'host'
+DB = 'database'
+
+SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
